452a30647cf9abb350d4508e3fe89877fac46961
Federico Sandrelli <Fed95@users.noreply.github.com>
2019-06-10 15:40:43 +0200

    Started BookJS
    about halfway...

--- assets/JS/bookJS.js
+++ assets/JS/bookJS.js
@@ -50,65 +50,160 @@ xhttp.send();
 var displayFoundBooks = function(books_list) {
 
     var parsed = JSON.parse(books_list);
-    //console.log("parsed: ", parsed[0]);
-    generateBookDiv(parsed);
+    console.log("parsed: ", parsed[0]);
 
+    var title = parsed[0].title;
+    var price = parsed[0].price;
+    var abstract = parsed[0].abstract;
     var authors = [];
-    var genres = [];
-    var reviews = [];
 
 
     for(var index in parsed){
-        authors.push(parsed[index]['author']);
-        genres.push(parsed[index]['genres']);
-        reviews.push(parsed[index]['reviews']);
+        authors.push(parsed[index].author);
     }
+
+    generateBookDiv(title, authors, price, abstract);
 };
 
 
 
 
 
-var generateBookDiv = function (title, authors, price) {
+var generateBookDiv = function (title, authors, price, abstract) {
 
-    var $div1 = $("<div />", {class : "list-group-item clearfix"});
+    var $div1 = $("<div class = 'row margin-top'/>");
     var $div2 = $("<div class = 'row'/>");
-    var $div3 = $("<div class = 'col-2 col-2-hidden-xs'>");
-    var $div4 = $("<div class = 'book-img'/>");
-    var $im = $("<img />", { src : "../assets/Images/BookCovers/Thumbnails/The BFG.jpg"});
-    var $div5 = $("<div class = 'col-8 col-8-bigger-xs'/>");
-    var $h = $("<h3 />", {id : 'title', class : 'book-title'}); $h.html(title);
-    var $div6 = $("<div class = 'book-info'/>");
-    var $div7 = $("<div class = 'col-3 info'/>");
-    var $s7 = $("<span />"); $s7.html('Authors:');
-    var $p7 = $("<p />"); $p7.html(authors);
-    var $space = $("<div class = 'col-0.5'/>");
-    var $div8 = $("<div class = 'col-3 info'/>");
-    var $s8 = $("<span />"); $s8.html('Price:');
-    var $p8 = $("<p />"); $p8.html(price + '$');
-    var $div9 = $("<div class = 'col-2 col-2-muchbigger-xs noleft-pad'>");
-    var $b9 = $("<button />", {id : 'add-book-btn-1', class : "btn btn-outline-success btn-add-book" , type : "input"});
-    $b9.html('Add to Cart')
+        var $col1 = $("<div class = 'col-1'/>");
+        var $div3 = $("<div class = 'col-10 singleItemContainer'/>");
+            var $div4 = $("<div class = 'col-3 singleItemContainer'/>");
+                var $im1 = $("<img />", { class:"singleItemImage big-screen-image", src:"../assets/Images/BookCovers/"+title+".jpg"});
+            var $col2 = $("<div class = 'col-1'/>");
+            var $div5 = $("<div class = 'col-8 singleItemContainer'/>");
+                var $h1 = $("<h1 class = 'singleItemName'/>"); $h1.html(title);
+                var $div6 = $("<div class = 'row small-screen-image' />");
+                    var $div7 = $("<div class = 'col-2' />");
+                    var $div8 = $("<div class = 'col-8' />");
+                        var $im2 = $("<img />", { class:"singleItemImage", src:"../assets/Images/BookCovers/"+title+".jpg"});
+                    var $div9 = $("<div class = 'col-2' />");
+                var $div11 = $("<div class = 'row authorrow' />");
+                    var $div12 = $("<div class = 'authorcontainer' />");
+                        var $a1 = $("<a href='' class='authorlink'/>"); $a1.html(authors);
+                var $span1 = $("<span />");
+                var $div13 = $("<div class = 'textcontent' />"); $div13.html(abstract);
+                var $hr1 = $("<hr>");
+                var $div14 = $("<div class = 'technical' />");
+                    var $p1 = $("<p />"); $p1.html("Publisher:");
+                    var $p2 = $("<p />"); $p2.html("Publication Date:");
+                    var $p3 = $("<p />"); $p3.html("Edition:");
+                    var $p4 = $("<p />"); $p4.html("Language:");
+                    var $p5 = $("<p />"); $p5.html("Number of Pages:");
+                    var $p6 = $("<p />"); $p6.html("ISBN:");
+                var $div15 = $("<div class = 'buybook' />");
+                    var $div16 = $("<div class = 'col-8' />");  $div16.html("Price: "+price+"$");
+                    var $div17 = $("<div class = 'col-4' />");
+                        var $b1 = $("<button id='add-book-btn-1' class='btn btn-outline-success btn-add-book' type='input'/>");
+                        $b1.html("Add to Cart");
 
+        var $col3 = $("<div class = 'col-1'/>");
 
-    $("#search-results-container").append($div1);
-    $div1.append($div2);
-    $div2.append($div3);
-    $div3.append($div4);
-    $div4.append($im);
-    $div2.append($div5);
-    $div5.append($h);
-    $div5.append($div6);
-    $div6.append($div7);
-    $div7.append($s7);
-    $div7.append($p7);
-    $div6.append($space);
-    $div6.append($div8);
-    $div8.append($s8);
-    $div8.append($p8);
-    $div2.append($div9);
-    $div9.append($b9);
-
+    $("#homepage-container").append($div1);
+        $div1.append($div2);
+        $div2.append($col1);
+        $div2.append($div3);
+            $div3.append($div4);
+                $div4.append($im1);
+            $div3.append($col2);
+            $div3.append($div5);
+                $div5.append($h1);
+                $div5.append($div6);
+                    $div6.append($div7);
+                    $div6.append($div8);
+                        $div8.append($im2);
+                    $div6.append($div9);
+                $div5.append($div11);
+                    $div11.append($div12);
+                        $div12.append($a1);
+                $div5.append($span1);
+                $div5.append($div13);
+                $div5.append($hr1);
+                $div5.append($div14);
+                    $div14.append($p1);
+                    $div14.append($p2);
+                    $div14.append($p3);
+                    $div14.append($p4);
+                    $div14.append($p5);
+                    $div14.append($p6);
+                $div5.append($div15);
+                    $div15.append($div16);
+                    $div15.append($div17);
+                        $div17.append($b1);
+            $div3.append($col3);
 };
 
 
+/*
+* <div class="row margin-top"></div>
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-10 singleItemContainer">
+                    <div class="col-3 singleItemContainer">
+                        <img class="singleItemImage big-screen-image"
+                             src="../assets/Images/BookCovers/game%20of%20thrones.jpg">
+                    </div>
+                    <div class="col-1 hid"></div>
+                    <div class="col-8 singleItemContainer">
+                        <h1 class="singleItemName">Game of Thrones</h1>
+                        <div class="row small-screen-image">
+                            <div class="col-2"></div>
+                            <div class="col-8">
+                                <img class="singleItemImage" src="../assets/Images/BookCovers/game%20of%20thrones.jpg">
+                            </div>
+                            <div class="col-2"></div>
+                        </div>
+                        <div class="row authorrow">
+                            <div class="authorcontainer">
+                                <a href="" class="authorlink">George R.R. Martin</a>
+                            </div>
+                            <div class="authorcontainer">
+                                <a href="" class="authorlink">Federico Sandrelli</a>
+                            </div>
+                            <div class="authorcontainer">
+                                <a href="" class="authorlink">Filippo Rezzonico</a>
+                            </div>
+                            <a href="" class="authorlink">Davide Santmabrogio</a>
+                        </div>
+                        <span></span>
+                        <div class="textcontent">It is a long established fact that a reader will be distracted by the
+                            readable content
+                            of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
+                            more-or-less
+                            normal
+                            distribution of letters, as opposed to using Content.
+                            It is a long established fact that a reader will be distracted by the readable content
+                            of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
+                            more-or-less
+                            normal
+                            distribution of letters, as opposed to using Content.
+                        </div>
+                        <hr>
+                        <div class="technical">
+                            <div><p>Publisher:</p></div>
+                            <div><p>Publication Date:</p></div>
+                            <div><p>Edition:</p></div>
+                            <div><p>Language:</p></div>
+                            <div><p>Number of Pages:</p></div>
+                            <div><p>ISBN:</p></div>
+                        </div>
+                        <div class="buybook">
+                            <div class="col-8">Price: 22$</div>
+                            <div class="col-4">
+                                <button id="add-book-btn-1" class="btn btn-outline-success btn-add-book" type="input">
+                                    Add to Cart
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-1 hid"></div>
+            </div>
+            * */
