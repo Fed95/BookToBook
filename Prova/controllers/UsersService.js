caf0cc6238a8b2e626303ba0e405085b0c263299
santadavo <davide2.santambrogio@mail.polimi.it>
2019-06-23 16:58:29 +0200

    update

--- Prova/controllers/UsersService.js
+++ Prova/controllers/UsersService.js
@@ -21,8 +21,15 @@ exports.postUser = function(args, res, next) {
    * parameters expected in the args:
   * body (User)
   **/
-  // no response value expected for this operation
-  res.end();
+    var examples = {};
+    if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
 }
 
 exports.postUserLogin = function(args, res, next) {
