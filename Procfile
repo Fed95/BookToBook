a3ddb3c1b8617e936c53ee16cd8f63ae98e4fb0d
santadavo <davide2.santambrogio@mail.polimi.it>
2019-06-23 16:15:37 +0200

    Prova post

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 // Procfile
 
-web: node ./New_Server/index.js
+web: node ./Prova/index.js
--- Prova/controllers/PurchasesService.js
+++ Prova/controllers/PurchasesService.js
@@ -47,8 +47,15 @@ exports.postPurchase = function(args, res, next) {
    * parameters expected in the args:
   * body (Purchase)
   **/
-  // no response value expected for this operation
-  res.end();
+    var examples = {};
+    if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
 }
 
 exports.postPurchaseCompleted = function(args, res, next) {
