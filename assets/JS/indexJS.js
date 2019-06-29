52724178d8f88b239a32df8fed7e43124b6224a6
Federico Sandrelli <Fed95@users.noreply.github.com>
2019-06-29 19:34:44 +0200

    added margin to p in footer

--- assets/JS/indexJS.js
+++ assets/JS/indexJS.js
@@ -114,3 +114,16 @@ function moveLeft() {
     }
 }
 
+var getCookie = function(name) {
+    var cookies = document.cookie.split(';');
+    for(var i=0 ; i < cookies.length ; ++i) {
+        var pair = cookies[i].trim().split('=');
+        if(pair[0] == name)
+            return pair[1];
+    }
+    return null;
+};
+
+c = getCookie('session')
+
+console.log(c)
