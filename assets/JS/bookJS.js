c533ff2982aca24df820a91eecbfb8262d4a3000
Federico Sandrelli <Fed95@users.noreply.github.com>
2019-06-29 16:08:14 +0200

    Link improvements

--- assets/JS/bookJS.js
+++ assets/JS/bookJS.js
@@ -64,7 +64,11 @@ var displayFoundBooks = function (book) {
 
     var interview = parsed[0].interview_text // TODO: DECIDE WHETHER THE INTERVIEW SHOULD BE AUTHOR SPECIFIC
 
-    for (var author in grouped_by_author) {
+    for(var author_name in grouped_by_author){
+        var author = {
+            name : author_name,
+            id : grouped_by_author[author_name][0].author_id
+        }
         authors.push(author);
     }
 
@@ -222,8 +226,8 @@ var addAuthors = function ($div, authors) {
     for (var i in authors) {
 
         var $d = $("<div class = 'authorcontainer' />");
-        var $a = $("<a href = '' class='authorlink'/>");
-        $a.html(authors[i]);
+        var $a = $("<a href = '"+ip+"pages/author.html?author_id="+authors[i].id+"' class='authorlink'/>");
+        $a.html(authors[i].name);
         $d.append($a);
         $div.append($d);
 
