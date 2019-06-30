ade7616d6ff9fe4b47fc678542557feb084532b2
Federico Sandrelli <Fed95@users.noreply.github.com>
2019-06-30 19:18:21 +0200

    Improved Author page

--- assets/JS/authorJS.js
+++ assets/JS/authorJS.js
@@ -152,169 +152,31 @@ var generateAuthorDiv = function (name, bio, books) {
                 $div5.append($hr1);
 
     if(books.length > 0){
-
-        var $d1 = $("<div class = 'row' />");
-        var $d2 = $("<div class = 'col-1' />");
-        var $d3 = $("<div class = 'col-10' />");
-        var $d4 = $("<div class = 'col-1' />");
-        var $hbook = $("<h3 />"); $hbook.html('Books');
-
-        $("#row-margin-top").append($d1);
-        $d1.append($d2);
-        $d1.append($d3);
-        $d3.append($hbook);
-        $d1.append($d4);
-
-        for(var i in books){
-            generateBookDiv(books[i]);
-        }
+        addSuggestedBooks(books)
     }
 };
 
-var generateBookDiv = function (book) {
+var addSuggestedBooks = function(books){
 
-    console.log('GENERATING BOOK DIVS')
-    /*
+    console.log(books)
 
-    var $diva = $("<div class='row'/>");
-        var $divb = $("<div class='col-1 hidden-s'/>");
-        var $divc = $("<div class='col-10 col-10-bigger-s'/>");
-            var $div1 = $("<div class = 'list-group-item' />");
-                var $div2 = $("<div class = 'row list-group-item'/>");
-                    var $div3 = $("<div class = 'col-2 col-2-hidden-xs'>");
-                        var $div4 = $("<div class = 'book-img'/>");
-                            var $im = $("<img />", { src : "../assets/Images/BookCovers/Thumbnails/"+book.title+".jpg"});
-                    var $div5 = $("<div class = 'col-8 col-8-bigger-xs'/>");
-                        var $h = $("<h3 />", {id : 'title', class : 'book-title'});
-                            var $a5 = $("<a />", {href : ip + 'pages/book.html?isbn='+book.isbn}); $a5.html(book.title);
-                        var $div6 = $("<div class = 'book-info'/>");
+    $("#row-margin-top").append('<div id="suggested-div" class="row suggested-books">')
+    $("#suggested-div").append('<h2>Written by the Author</h2>')
 
+    for (var i in books) {
 
-     */
+        $('#suggested-div').append(
 
-    $("#row-margin-top").append(
-        '<div class="row">' +
-            '<div class="col-1 hidden-s"/>' +
-            '<div class="col-10 col-10-bigger-s">' +
-                '<div class = "list-group-item" >' +
-    '                <div class = "row list-group-item">' +
-    '                    <div class = "col-2 col-2-hidden-xs">' +
-    '                        <div class = "book-img">' +
-    '                            <img src = "../assets/Images/BookCovers/Thumbnails/'+book.title+'.jpg" />' +
-                            '</div>'+
-                        '</div>'+
-    '                    <div class = "col-8 col-8-bigger-xs">' +
-    '                        <h3 id = "title" class = "book-title">' +
-    '                            <a href = "'+ip+'pages/book.html?isbn='+book.isbn+'">'+book.title+'' +
-                            '</h3>'+
-    '                        <div class = "book-info"></div>'+
-                        '</div>'+
-                    '</div>'+
-                 '</div>'+
-            '</div>'+
-        '</div>'
-    );
-/*
-
-    $("#row-margin-top").append($diva);
-    $diva.append($divb);
-    $diva.append($divc);
-    $divc.append($div1);
-    $div1.append($div2);
-    $div2.append($div3);
-    $div3.append($div4);
-    $div4.append($im);
-    $div2.append($div5);
-    $div5.append($h);
-    $h.append($a5);
-    $div5.append($div6);
-    /*
-    $div6.append($div7);
-    $div7.append($s7);
-    $div7.append($p7);
+            '<div class="col-sm singleBook">' +
+            '<div class="description">' +
+            '<a href="'+ip+'pages/book.html?isbn='+books[i].isbn+'">'+
+            '<img class="singleItemImage" src="../assets/Images/BookCovers/' + books[i].title + '.jpg"/>' +
+            '<h3>' + books[i].title + '</h3>' +
+            '</a>'+
+            '</div>' +
+            '</div>'
+        );
+    }
+}
 
 
-    $div6.append($div8);
-    $div8.append($s8);
-    $div8.append($p8);
-    $div2.append($div10);
-    $div10.append($b10);
-*/
-    console.log('looking for author of ' + book.title);
-
-    //todo: need to generate a new request each time to prevent overlap of gets
-    //xhttpAuthors.open("GET", ip + "api/author/findByBook?ISBN="+book.isbn, true);
-    //xhttpAuthors.send();
-
-};
-
-/*<div class="row margin-top"></div>
-                    <div class="row">
-                        <div class="col-1"></div>
-                        <div class="col-10 singleItemContainer">
-                            <div class="col-3 singleItemContainer">
-                                <img class="singleItemImage big-screen-image" src="../assets/Images/AuthorPictures/Michael%20Crichton.jpg">
-                            </div>
-                            <div class="col-1 hid"></div>
-                            <div class=" col-8 singleItemContainer">
-                                <h1 class="singleItemName">Michael Crichton</h1>
-                                <div class="row small-screen-image">
-                                    <div class="col-2"></div>
-                                    <div class="col-8">
-                                        <img class="singleItemImage" src="../assets/Images/AuthorPictures/Michael%20Crichton.jpg">
-                                    </div>
-                                    <div class="col-2"></div>
-                                </div>
-                                <div class="row">
-                                    <p class="datestyle">23/10/1942 - 4/11/2008</p>
-                                </div>
-                                <div class="textcontent">It is a long established fact that a reader will be distracted by the readable content
-                                    of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less
-                                    normal
-                                    distribution of letters, as opposed to using Content.
-                                    It is a long established fact that a reader will be distracted by the readable content
-                                    of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less
-                                    normal
-                                    distribution of letters, as opposed to using Content.
-                                </div>
-                                <hr>
-                            </div>
-                        </div>
-                        <div class="col-1 hid"></div>
-                    </div>
-                    <div class="row">
-                        <div class="col-1"></div>
-                        <div class="col-10"><h3>Books</h3></div>
-                        <div class="col-1"></div>
-                    </div>
-                    <div class="row">
-                        <div class="col-1 hidden-s"></div>
-                        <div class="col-10 col-10-bigger-s">
-                            <div class="list-group-item clearfix">
-                                <div class="row">
-                                    <div class="col-2 col-2-hidden-xs">
-                                        <div class="book-img"><img src="../assets/Images/BookCovers/Thumbnails/Prey.jpg"></div>
-                                    </div>
-                                    <div class="col-8 col-8-bigger-xs">
-                                        <h3 id="title" class="book-title">Prey</h3>
-                                        <div class="book-info">
-                                            <div class="col-5 info">
-                                                <span>Authors:</span>
-                                                <p>Filippo Rezzonico</p>
-                                                <p>Michael Crichton</p>
-                                            </div>
-                                            <div class="col-5 info">
-                                                <span>Price:</span>
-                                                <p>3000$</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-2 col-2-muchbigger-xs noleft-pad">
-                                    <button id="add-book-btn-1" class="btn btn-outline-success btn-add-book" type="input">Add to Cart</button>
-                                    </div>
-                                </div>
-                        </div>
-                        <div class="col-1 hidden-s"></div>
-                    </div>
-
-                    <div class="row margin-bottom"></div>*/
